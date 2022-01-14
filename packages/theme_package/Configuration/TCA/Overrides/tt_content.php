630c6d357a00c34c20b83ca85c612805c9a8f37f
Beck Thomas <tbeck@sqli.com>
2022-01-14 13:02:23 +0100

    add animation

---
+++ packages/theme_package/Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,11 @@
+<?php
+defined('TYPO3') or die();
+
+$GLOBALS['TCA']['tt_content']['columns']['header_layout']['label'] = 'LLL:EXT:theme_package/Resources/Private/Language/locallang.xlf:tt_content.title_size';
+$GLOBALS['TCA']['tt_content']['columns']['header_layout']['config']['items']['1'] = array('LLL:EXT:theme_package/Resources/Private/Language/locallang.xlf:tt_content.title_size1','1');
+$GLOBALS['TCA']['tt_content']['columns']['header_layout']['config']['items']['2'] = array('LLL:EXT:theme_package/Resources/Private/Language/locallang.xlf:tt_content.title_size2','2');
+$GLOBALS['TCA']['tt_content']['columns']['header_layout']['config']['items']['3'] = array('LLL:EXT:theme_package/Resources/Private/Language/locallang.xlf:tt_content.title_size3','3');
+$GLOBALS['TCA']['tt_content']['columns']['header_layout']['config']['items']['4'] = array('LLL:EXT:theme_package/Resources/Private/Language/locallang.xlf:tt_content.title_size4','4');
+$GLOBALS['TCA']['tt_content']['columns']['header_layout']['config']['items']['5'] = array('LLL:EXT:theme_package/Resources/Private/Language/locallang.xlf:tt_content.title_size5','5');
+$GLOBALS['TCA']['tt_content']['columns']['header_layout']['config']['items']['6'] = array('LLL:EXT:theme_package/Resources/Private/Language/locallang.xlf:tt_content.title_size6','6');
+$GLOBALS['TCA']['tt_content']['columns']['header_layout']['config']['items']['7'] = array('LLL:EXT:theme_package/Resources/Private/Language/locallang.xlf:tt_content.title_hidden','100');
--- packages/theme_slcreation/ext_localconf.php
+++ packages/theme_slcreation/ext_localconf.php
@@ -6,6 +6,6 @@ defined('TYPO3') or die();
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['theme_slcreation'] = 'EXT:theme_slcreation/Configuration/RTE/rte.yaml';
 
 // Add color to page tree.
-// \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig('
-//   options.pageTree.backgroundColor.1 = rgba(255,109,0,0.2)
-// ');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig('
+  options.pageTree.backgroundColor.2 = rgba(13,202,240,0.2)
+');
