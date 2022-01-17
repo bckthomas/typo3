e65f056aa7413cc04bc74219265247ff6b7b25e3
Beck Thomas <tbeck@sqli.com>
2022-01-17 16:05:01 +0100

    Add cke for rte

--- packages/theme_slcreation/ext_localconf.php
+++ packages/theme_slcreation/ext_localconf.php
@@ -3,7 +3,7 @@
 // Prevent script from being called directly
 defined('TYPO3') or die();
 
-$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['theme_slcreation'] = 'EXT:theme_slcreation/Configuration/RTE/rte.yaml';
+$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['default'] = 'EXT:theme_slcreation/Configuration/RTE/rte.yaml';
 
 // Add color to page tree.
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig('
