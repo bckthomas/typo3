2bea7c6f9ed1844df0eeb63993ded6e3a51827f1
Beck Thomas <tbeck@sqli.com>
2022-02-09 09:30:13 +0100

    Add staticfile extension

--- public/typo3conf/AdditionalConfiguration.php
+++ public/typo3conf/AdditionalConfiguration.php
@@ -2,106 +2,54 @@
 
 switch (\TYPO3\CMS\Core\Core\Environment::getContext()) {
   case 'Development':
-    $GLOBALS['TYPO3_CONF_VARS'] = array_replace_recursive(
-      $GLOBALS['TYPO3_CONF_VARS'],
-        [
-          'DB' => [
-              'Connections' => [
-                  'Default' => [
-                      'dbname' => 'db',
-                      'host' => 'ddev-kwible-db',
-                      'password' => 'db',
-                      'user' => 'db',
-                  ],
-              ],
-          ],
-          // This GFX configuration allows processing by installed ImageMagick 6
-          'GFX' => [
-              'processor' => 'ImageMagick',
-              'processor_path' => '/usr/bin/',
-              'processor_path_lzw' => '/usr/bin/',
-          ],
-          // This mail configuration sends all emails to mailhog
-          'MAIL' => [
-              'transport' => 'smtp',
-              'transport_smtp_server' => 'localhost:1025',
-          ],
-          'SYS' => [
-              'trustedHostsPattern' => '.*.*',
-              'devIPmask' => '*',
-              'displayErrors' => 1,
-              'exceptionalErrors' => '12290',
-          ],
-          'BE' => [
-              'debug' => true,
-          ],
-          'FE' => [
-              'debug' => true,
-          ],
-          'LOG' => [
-              'TYPO3' => [
-                  'CMS' => [
-                      'deprecations' => [
-                          'writerConfiguration' => [
-                              'notice' => [
-                                  'TYPO3\CMS\Core\Log\Writer\FileWriter' => [
-                                      'disabled' => false,
-                                  ],
-                              ],
-                          ],
-                      ],
-                  ],
-              ],
-          ],
-        ]
-    );
+    $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['user'] = 'db';
+    $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['password'] = 'db';
+    $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['host'] = 'ddev-kwible-db';
+    $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['dbname'] = 'db';
 
-    if(file_exists(dirname(__FILE__).'./productionDatabase.php')){
-        require_once(dirname(__FILE__).'./productionDatabase.php');
+    $GLOBALS['TYPO3_CONF_VARS']['GFX']['processor'] = 'ImageMagick';
+    $GLOBALS['TYPO3_CONF_VARS']['GFX']['processor_path'] = '/usr/bin/';
+    $GLOBALS['TYPO3_CONF_VARS']['GFX']['processor_path_lzw'] = '/usr/bin/';
+
+    $GLOBALS['TYPO3_CONF_VARS']['MAIL']['transport'] = 'smtp';
+    $GLOBALS['TYPO3_CONF_VARS']['MAIL']['transport_smtp_server'] = 'localhost:1025';
+
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['trustedHostsPattern'] = '.*';
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['devIPmask'] = '*';
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['displayErrors'] = '1';
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['exceptionalErrors'] = '12290';
+
+    $GLOBALS['TYPO3_CONF_VARS']['BE']['debug'] = true;
+
+    $GLOBALS['TYPO3_CONF_VARS']['FE']['debug'] = true;
+
+    $GLOBALS['TYPO3_CONF_VARS']['LOG']['TYPO3']['CMS']['deprecations']['writerConfiguration']['notice']['TYPO3\CMS\Core\Log\Writer\FileWriter']['disabled'] = false;
+
+    if(file_exists(dirname(__FILE__).'/productionDatabase.php')){
+      require_once(dirname(__FILE__).'/productionDatabase.php');
     }
 
     break;
 
   case 'Production':
-    $GLOBALS['TYPO3_CONF_VARS'] = array_replace_recursive(
-        $GLOBALS['TYPO3_CONF_VARS'],
-        [
-          'BE' => [
-            'versionNumberInFilename' => '1',
-            'debug' => false,
-          ],
-          'FE' => [
-              'debug' => false,
-          ],
-          'LOG' => [
-            'TYPO3' => [
-              'CMS' => [
-                'deprecations' => [
-                  'writerConfiguration' => [
-                    'notice' => [
-                      'TYPO3\CMS\Core\Log\Writer\FileWriter' => [
-                        'disabled' => true,
-                      ],
-                    ],
-                  ],
-                ],
-              ],
-            ],
-          ],
-          'SYS' => [
-            'devIPmask' => '',
-            'displayErrors' => 0,
-            'exceptionalErrors' => '12290',
-            'enableDeprecationLog' => '0',
-            'syslogErrorReporting' => '0',
-            'belogErrorReporting' => '0',
-            'sqlDebug' => '0',
-            'no_pconnect' => '1',
-          ],
-        ]
-    );
-    if(file_exists(dirname(__FILE__).'./productionDatabase.php')){
-        require_once(dirname(__FILE__).'./productionDatabase.php');
+
+    $GLOBALS['TYPO3_CONF_VARS']['BE']['debug'] = false;
+
+    $GLOBALS['TYPO3_CONF_VARS']['FE']['debug'] = false;
+
+    $GLOBALS['TYPO3_CONF_VARS']['LOG']['TYPO3']['CMS']['deprecations']['writerConfiguration']['notice']['TYPO3\CMS\Core\Log\Writer\FileWriter']['disabled'] = true;
+
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['devIPmask'] = 0;
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['displayErrors'] = '0';
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['exceptionalErrors'] = '12290';
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['enableDeprecationLog'] = '0';
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['syslogErrorReporting'] = '0';
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['belogErrorReporting'] = '0';
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['sqlDebug'] = '0';
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['no_pconnect'] = '1';
+
+    if(file_exists(dirname(__FILE__).'/productionDatabase.php')){
+        require_once(dirname(__FILE__).'/productionDatabase.php');
     }
     break;
 }
