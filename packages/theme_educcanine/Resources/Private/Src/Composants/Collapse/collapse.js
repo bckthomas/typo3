c56f50418202c0f57cb52501abb6e281e5a4b3de
Beck Thomas <tbeck@sqli.com>
2023-02-14 17:56:51 +0100

    Replace gulp with npm scripts

--- packages/theme_educcanine/Resources/Private/Src/Composants/Collapse/collapse.js
+++
@@ -1,6 +0,0 @@
-import Dropdown from '../../../../../node_modules/bootstrap/js/dist/collapse';
-import Event from '../../../../../node_modules/bootstrap/js/dist/dom/event-handler';
-import Manipulator from '../../../../../node_modules/bootstrap/js/dist/dom/manipulator';
-import Selector from '../../../../../node_modules/bootstrap/js/dist/dom/selector-engine';
-import Base from '../../../../../node_modules/bootstrap/js/dist/base-component';
-import Data from '../../../../../node_modules/bootstrap/js/dist/dom/data';
