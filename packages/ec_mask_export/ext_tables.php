4af367e4a91d4113dda3f13ac9f4d88be1f6aba9
Beck Thomas <tbeck@sqli.com>
2023-05-12 15:38:33 +0200

    Delete ec_mask_export

--- packages/ec_mask_export/ext_tables.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('TYPO3_MODE') || die();
-
-call_user_func(static function () {
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_ecmaskexport_prices, tx_ecmaskexport_squares, tx_ecmaskexport_texts');
-
-});
-
