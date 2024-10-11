d4d752c61ced13646910a704d63e8a05511b5ec2
Thomas Beck <contact@thomas-beck.fr>
2024-10-11 13:22:07 +0200

    initv13

--- packages/theme_mask/ext_emconf.php
+++ packages/theme_mask/ext_emconf.php
@@ -11,8 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '0.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.0.0-12.99.99',
-            'mask' => '8.0.0-9.0.0'
+            'typo3' => '13.0.0-13.99.99',
         ]
     ],
     'autoload' => [
